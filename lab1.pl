d2f9a8dd3089c6c5a962796af36c0613b5c1e2ff
default187 <117059720+default187@users.noreply.github.com>
2024-02-21 16:53:53 +0800

    Add files via upload
    Лаб1, задание 6

---
+++ lab1.pl
@@ -0,0 +1,25 @@
+%Медведь большой. Слон большой. Кот маленький
+big(bear).
+big(elephant).
+small(cat).
+
+%Медведь коричневый. Слон серый. Кот черный
+color(bear, brown).
+color(cat, black).
+color(elephant, gray).
+
+%Любой черный или коричневый предмет является темным
+dark(X) :- color(X, brown) ; color(X, black).
+
+%Кто одновременно большой и темный?
+?-dark(Y), big(Y).
+
+%Есть ли коричневые маленькие слоны?
+?-color(elephant, brown), small(elephant).
+
+%Есть ли большие и темные медведи?
+?-big(bear), dark(bear).
+
+%Есть ли черный кот?
+?-color(cat, black).
+
