7770b1ce1e51ad052217edc04f56cab550b740c1
default187 <117059720+default187@users.noreply.github.com>
2024-03-22 15:39:20 +0800

    Create lab2.pl

---
+++ lab2.pl
@@ -0,0 +1,6 @@
+replace(_, _, [], []).
+replace(Element, 0, [_|Tail], [Element|Tail]).
+replace(Element, Index, [Head|Tail], [Head|NewTail]) :-
+    Index > 0,
+    NextIndex is Index - 1,
+    replace(Element, NextIndex, Tail, NewTail).
